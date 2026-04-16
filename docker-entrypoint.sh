5343c2f491f56a56af687d79f1085db6cb3aef76
Chaal Pritam <chaalpritam@gmail.com>
2026-04-16 14:26:23 +0530

    revert: remove entrypoint, restore original Dockerfile

--- docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# If SERVER_WALLET_JSON is set (base64), decode it to the wallet file path
-if [ -n "$SERVER_WALLET_JSON" ]; then
-  echo "$SERVER_WALLET_JSON" | base64 -d > "${SERVER_WALLET_PATH:-/app/server-wallet.json}"
-fi
-exec "$@"
